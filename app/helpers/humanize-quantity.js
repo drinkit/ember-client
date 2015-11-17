c09d0ecd72f1c8101ff1c320ce83451de9e9b6de
Crabar <crabar0@gmail.com>
2015-11-17 10:13:14 +0200

    added quantity helper;

---
+++ app/helpers/humanize-quantity.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export function humanizeQuantity(quantity) {
+  return quantity ? quantity + ' мл' : 'немного';
+}
+
+export default Ember.Helper.helper(humanizeQuantity);
---
+++ tests/unit/helpers/humanize-quantity-test.js
@@ -0,0 +1,10 @@
+import { humanizeQuantity } from '../../../helpers/humanize-quantity';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | humanize quantity');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = humanizeQuantity(42);
+  assert.ok(result);
+});
