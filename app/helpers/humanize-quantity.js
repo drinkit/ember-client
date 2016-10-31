10541cf087f18edc97f0a9910a0dce0616a2dc76
Crabar <crabar0@gmail.com>
2016-10-31 22:50:48 +0200

    added backward compatibility for units

--- app/helpers/humanize-quantity.js
+++ app/helpers/humanize-quantity.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function humanizeQuantity([quantity, unit]) {
-  return quantity ? quantity + ' ' + unit : 'немного';
+  return quantity ? quantity + ' ' + (unit ? unit : 'мл') : 'немного';
 }
 
 export default Ember.Helper.helper(humanizeQuantity);
