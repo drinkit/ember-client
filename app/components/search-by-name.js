06ce8a7a2f6d2762b744ba7629d4631716b2b3a3
Crabar <crabar0@gmail.com>
2016-12-17 21:35:34 +0200

    burger init commit

--- app/components/search-by-name.js
+++ app/components/search-by-name.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  burgerMenu: Ember.inject.service(),
+  classNames: ['search-box'],
   actions: {
     search: function(value) {
       this.sendAction('search', value);
       $('input[type="search"]').blur();
+      this.get('burgerMenu').toggleProperty('open');
     }
   }
 });
