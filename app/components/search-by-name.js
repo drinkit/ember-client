43867674f5f388d6bdaf5aaff4dd332d0f30c8f4
Crabar <crabar0@gmail.com>
2016-12-25 17:39:22 +0200

    worked hard

--- app/components/search-by-name.js
+++ app/components/search-by-name.js
@@ -2,12 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   burgerMenu: Ember.inject.service(),
-  classNames: ['search-box'],
+  classNames: ['search-by-name', 'form-group'],
   actions: {
     search: function(value) {
       this.sendAction('search', value);
       $('input[type="search"]').blur();
-      this.get('burgerMenu').toggleProperty('open');
     }
   }
 });
