65473bcf4610df1355c1fdb5d2d3242ad86fccc8
Crabar <crabar0@gmail.com>
2018-03-15 19:44:46 +0200

    added tracking for ingredients pages

--- app/routes/ingredient.js
+++ app/routes/ingredient.js
@@ -26,4 +26,26 @@ export default Ember.Route.extend({
       this.set('headData.description', ingredient.description);
     });
   },
+
+  actions: {
+    didTransition() {
+      const repository = this.get('repository');
+      repository.findOne('ingredient', this.get('currentModel').ingredientId).then((ingredient) => {
+        Ember.run.scheduleOnce('afterRender', this, () => {
+          const page = document.location.pathname;
+          const title = ingredient.get('name');
+          Ember.get(this, 'metrics').trackPage({
+            page,
+            title
+          });
+        });
+      });
+    },
+
+    error(error, transition) {
+      if (error && error.status === 404) {
+        this.transitionTo('/error404');
+      }
+    }
+  }
 });
