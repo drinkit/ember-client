0b120653934b72fef84cd22bc79a97d434eed702
Crabar <crabar0@gmail.com>
2020-01-07 18:22:17 +0200

    added og image to head

--- config/environment.js
+++ config/environment.js
@@ -8,11 +8,11 @@ module.exports = function(environment) {
     locationType: 'auto',
     contentSecurityPolicy: {
       'frame-src': "'self' oauth.vk.com",
-      'font-src': "'self' https://fonts.gstatic.com/l/font",
-      'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com",
-      'script-src': "'self' www.google-analytics.com api.vk.com",
-      'connect-src': "'self' http://localhost:8080 www.google-analytics.com https://aws-server.drinkit.guru https://drinkit-stg.eu-central-1.elasticbeanstalk.com/ https://server.drinkit.guru https://graph.facebook.com",
-      'img-src': "'self' https://server.drinkit.guru https://s3.eu-central-1.amazonaws.com https://drinkit-stg.eu-central-1.elasticbeanstalk.com/ www.google-analytics.com data:"
+      'font-src': "'self' https://fonts.gstatic.com/l/font https://aws-server.drinkit.guru",
+      'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com https://aws-server.drinkit.guru",
+      'script-src': "'self' www.google-analytics.com api.vk.com https://aws-server.drinkit.guru",
+      'connect-src': "'self' https://static.78.49.203.116.clients.your-server.de http://localhost:8080 www.google-analytics.com https://aws-server.drinkit.guru https://drinkit-stg.eu-central-1.elasticbeanstalk.com/ https://server.drinkit.guru https://graph.facebook.com",
+      'img-src': "'self' https://static.78.49.203.116.clients.your-server.de https://server.drinkit.guru https://s3.eu-central-1.amazonaws.com https://drinkit-stg.eu-central-1.elasticbeanstalk.com/ www.google-analytics.com data:"
     },
     EmberENV: {
       FEATURES: {
@@ -52,7 +52,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV['server-path'] = 'https://aws-server.drinkit.guru';
+    ENV['server-path'] = 'https://static.78.49.203.116.clients.your-server.de';
   }
 
   if (environment === 'test') {
@@ -68,7 +68,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV['server-path'] = 'https://aws-server.drinkit.guru';
+    ENV['server-path'] = 'https://static.78.49.203.116.clients.your-server.de';//'https://aws-server.drinkit.guru';
   }
 
   return ENV;
