a24bd464cdba5e51ff6125498236a6177c955653
Yaroslav Poliakov <yaroslav.poliakov@bolt.eu>
2022-04-17 13:12:43 +0300

    updated a few controllers

--- app/controllers/ingredient.js
+++ app/controllers/ingredient.js
@@ -1,4 +1,3 @@
-import { observer } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
--- app/controllers/ingredients.js
+++ app/controllers/ingredients.js
@@ -1,37 +1,38 @@
-import { observer } from '@ember/object';
 import Controller from '@ember/controller';
 
-export default Controller.extend({
-  categories: ['Крепкие алкогольные напитки', 'Ликеры', 'Слабоалкогольные напитки', 'Безалкогольные напитки', 'Прочее'],
-  ingredientsInCategories: {
-    'Крепкие алкогольные напитки': [],
-    'Ликеры': [],
-    'Слабоалкогольные напитки': [],
-    'Безалкогольные напитки': [],
-    'Прочее': []
-  },
+export default class IngredientsController extends Controller {
+  categories = ['Крепкие алкогольные напитки', 'Ликеры', 'Слабоалкогольные напитки', 'Безалкогольные напитки', 'Прочее'];
 
   clear() {
     const ingredientsInCategories = this.get('ingredientsInCategories');
-    for (var category in ingredientsInCategories) {
-      if (ingredientsInCategories.hasOwnProperty(category)) {
+    for (let category in this.ingredientsInCategories) {
+      if (this.ingredientsInCategories.hasOwnProperty(category)) {
         ingredientsInCategories[category] = [];
       }
     }
-  },
+  }
 
-  modelChanged: observer('model', function() {
-    this.clear();
-    const ingredientsInCategories = this.get('ingredientsInCategories');
-    this.get('model').forEach((item) => {
-      let category = item.get('category');
-      if (category) {
-        ingredientsInCategories[item.get('category')].push(item);
+  get ingredientsInCategories() {
+    const res = {
+      'Крепкие алкогольные напитки': [],
+      'Ликеры': [],
+      'Слабоалкогольные напитки': [],
+      'Безалкогольные напитки': [],
+      'Прочее': []
+    };
+
+    const ingredients = this.model.toArray();
+
+    for (const ingredient of ingredients) {
+      if (ingredient.get('category')) {
+        res[ingredient.get('category')].push(ingredient);
       }
-    });
-
-    for (var category in ingredientsInCategories) {
-      ingredientsInCategories[category].sort((a, b) => a.get('name') < b.get('name') ? -1 : 1);
     }
-  })
-});
+
+    for (let category in res) {
+      res[category].sort((a, b) => a.get('name') < b.get('name') ? -1 : 1);
+    }
+
+    return res;
+  }
+}
